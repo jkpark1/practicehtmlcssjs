ea5ef2c61bd54417e3df8de894892e17e940d5b4
jkpark1 <stw369@gmail.com>
2023-02-15 16:41:42 +0900

    Change Start stop reset btn Func a

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -1,5 +1,4 @@
 const button = document.querySelectorAll("button");
-const STbutton = document.querySelectorAll(".ST-button");
 const startBtn = button[0];
 const pauseBtn = button[1];
 const resetBtn = button[2];
@@ -7,13 +6,10 @@ const FcsFTBtn = button[3];
 const FcsHTBtn = button[4];
 const BrkTBtn = button[5];
 
-// const MinU = STbutton[0];          --Deleted Operations
-// const MinD = STbutton[1];
-// const SecU = STbutton[2];
-// const SecD = STbutton[3];          --Deleted Operations
-
 ///Timer Rules
 let countdown = 15;
+let ResetCnDw = 15;
+let BreakCnDw = 0;
 let min = "";
 let sec = "";
 min = parseInt(countdown / 60);
@@ -23,57 +19,31 @@ let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
 let timer; ///This is Start Timer Function
 let AnimeWork;
 var Water = document.getElementById("WaterAnime");
-var pos = 105;
-// let HoldMU;
-// let HoldMD;
-// let HoldSU;
-// let HoldSD;
+var AnimeTop = 105;
+
 const timerDisplay = document.getElementById("pomodoroclock");
 timerDisplay.innerHTML = display;
 
-///Control Timer Buttons
+///Buttons : Control Timer
 ///startBtn : Click 'Start'button on page Timer start
 ///pauseBtn : Click 'Pause'button on page Timer pause  and click 'Start'button ,after click 'Pause'Button, Timer resume
 ///resetBtn : Click 'Reset'button on page Timer stop and reset
+
+///Timer : initial vlaue
 startBtn.addEventListener("click", startTimer);
 
-pauseBtn.addEventListener("click", pauseTimer);
-resetBtn.addEventListener("click", resetTimer);
-///Quick Set Time Buttons
+///Buttons : Quick Set Time
 FcsFTBtn.addEventListener("click", setTimer50m);
 FcsHTBtn.addEventListener("click", setTimer25m);
-///Detail Set Time Buttons                          --Deleted Operations
-// MinU.addEventListener("click", OneMinUp);
-// MinD.addEventListener("click", OneMinDW);
-// SecU.addEventListener("click", OneSecUp);
-// SecD.addEventListener("click", OneSecDW);
-///Fast Set Time Up and Down Buttons
-// MinU.addEventListener("mousedown", holdMU);
-// MinU.addEventListener("mouseup", RelMinUp);
-// MinD.addEventListener("mousedown", holdMD);
-// MinD.addEventListener("mouseup", RelMinDW);
-// SecU.addEventListener("mousedown", holdSU);
-// SecU.addEventListener("mouseup", RelSecUp);
-// SecD.addEventListener("mousedown", holdSD);
-// SecD.addEventListener("mouseup", RelSecDW);      --Deleted Operations
+BrkTBtn.addEventListener("click", BreakTime);
 
 function startTimer() {
   timer = setInterval(pomodorotimer, 1000);
   AnimeWork = setInterval(AnimeRule, 50);
   startBtn.removeEventListener("click", startTimer);
+  pauseBtn.addEventListener("click", pauseTimer);
+  resetBtn.addEventListener("click", resetTimer);
 }
-// function holdMU() {                            --Deleted Operations
-//   HoldMU = setInterval(HoldMinUp, 100);
-// }
-// function holdMD() {
-//   HoldMD = setInterval(HoldMinDW, 100);
-// }
-// function holdSU() {
-//   HoldSU = setInterval(HoldSecUp, 100);
-// }
-// function holdSD() {
-//   HoldSD = setInterval(HoldSecDW, 100);
-// }                                              --Deleted Operations
 
 function pauseTimer() {
   clearInterval(timer);
@@ -83,10 +53,10 @@ function pauseTimer() {
 function resetTimer() {
   pauseTimer();
   AnimeReset();
-  startBtn.addEventListener("click", startTimer);
+  resetBtn.removeEventListener("click", resetTimer);
 
-  pos = 105;
-  countdown = 1500;
+  AnimeTop = 105;
+  countdown = ResetCnDw;
   min = parseInt(countdown / 60);
   sec = countdown % 60;
   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
@@ -94,8 +64,10 @@ function resetTimer() {
 }
 function setTimer50m() {
   clearInterval(timer);
-
-  countdown = 3000;
+  resetTimer();
+  countdown = 10;
+  ResetCnDw = 10;
+  BreakCnDw = 2;
 
   min = parseInt(countdown / 60);
   sec = countdown % 60;
@@ -104,124 +76,32 @@ function setTimer50m() {
 }
 function setTimer25m() {
   clearInterval(timer);
-
-  countdown = 1500;
-
-  min = parseInt(countdown / 60);
-  sec = countdown % 60;
-  let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-  timerDisplay.innerHTML = display;
-}
-
-function setTimer5m() {
-  clearInterval(timer);
-
+  resetTimer();
   countdown = 5;
-
+  ResetCnDw = 5;
+  BreakCnDw = 1;
   min = parseInt(countdown / 60);
   sec = countdown % 60;
   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
   timerDisplay.innerHTML = display;
 }
-function setTimer10m() {
+
+function BreakTime() {
   clearInterval(timer);
-
-  countdown = 600;
-
-  min = parseInt(countdown / 60);
+  countdown = BreakCnDw;
+  countdown = min = parseInt(countdown / 60);
   sec = countdown % 60;
   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
   timerDisplay.innerHTML = display;
 }
 
-// function OneMinUp() {                                                              --Deleted Operations
-//   countdown = countdown + 60;
-
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function OneMinDW() {
-//   if (countdown > 0) {
-//     countdown = countdown - 60;
-//   }
-
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function OneSecUp() {
-//   countdown = countdown + 1;
-
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function OneSecDW() {
-//   if (countdown > 0) {
-//     countdown = countdown - 1;
-//   }
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function HoldMinUp() {
-//   countdown = countdown + 60;
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function HoldMinDW() {
-//   if (countdown > 0) {
-//     countdown = countdown - 60;
-//   }
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }                                                                                  --Deleted Operations
-
-//function RelMinUp() {
-//  clearInterval(HoldMU);
-//}
-//function RelMinDW() {
-//  clearInterval(HoldMD);
-//};
-
-// This is ERROR code                                                                  --Deleted Operations
-// function HoldSecUp() {
-//   countdown = countdown + 1;
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function HoldSecDW() {
-//   if (countdown > 0) {
-//     countdown = countdown - 1;
-//   }
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
-//   let display = `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
-//   timerDisplay.innerHTML = display;
-// }
-// function RelSecUp() {
-//   clearInterval(HoldSU);
-// }
-// function RelSecDW() {
-//   clearInterval(HoldSD);
-//}This is ERROR code                                                                   --Deleted Operations
+///Cal Anime Value : initial top(value) = 105px, target top = -155px, total = 260px box move initial pos
 function AnimeRule() {
-  if (pos < -155) {
+  if (AnimeTop < -155) {
     clearInterval(AnimeWork);
   } else {
-    pos = pos - 0.867;
-    Water.style.top = pos + "px";
+    AnimeTop = AnimeTop - 0.867;
+    Water.style.top = AnimeTop + "px";
   }
 }
 function AnimeReset() {
@@ -243,13 +123,7 @@ function pomodorotimer() {
     AlarmSounds1.play();
     timerDisplay.innerText = "END";
     setTimeout(() => {
-      countdown = 15;
-      min = parseInt(countdown / 60);
-      sec = countdown % 60;
-      let display = `${min < 10 ? "0" + min : min}:${
-        sec < 10 ? "0" + sec : sec
-      }`;
-      timerDisplay.innerText = display;
+      resetTimer();
     }, 3000);
   }
 }
