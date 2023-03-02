5a7f141f84b3aef5bb2c4ece7ad41e3e7fefbcbc
jkpark1 <stw369@gmail.com>
2023-03-02 16:57:50 +0900

    Add Testcode Fix Animations

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -9,9 +9,10 @@ const icon = document.querySelectorAll(".bi");
 const timerDisplay = document.getElementById("pomodoroclock");
 ///Timer Rules
 ///initial Value
-let countdown = 1500;
-let ResetCnDw = 1500;
-let BreakCnDw;
+// let countdown = 1500;
+// let ResetCnDw = 1500;
+let countdown = 10;
+let ResetCnDw = 10;
 let min = "";
 let sec = "";
 min = parseInt(countdown / 60);
@@ -27,7 +28,7 @@ var Alarm = new Audio("AlarmSound1.mp3");
 let timer; ///This is Start Timer Function
 let AnimeWork;
 var Water = document.getElementById("WaterAnime");
-var AnimeTop = 105;
+var AnimeTop = 250;
 
 ///Buttons : Control Timer
 ///startBtn : Click 'Start'button on page Timer start
@@ -45,6 +46,7 @@ function startTimer() {
   timer = setInterval(pomodorotimer, 1000);
   AnimeWork = setInterval(AnimeRule, 50);
   StartColor();
+  WaterDrop();
   startBtn.removeEventListener("click", startTimer);
   pauseBtn.addEventListener("click", pauseTimer);
   resetBtn.addEventListener("click", resetTimer);
@@ -53,6 +55,7 @@ function startTimer() {
 function pauseTimer() {
   clearInterval(timer);
   clearInterval(AnimeWork);
+  WaterDropStop();
   PauseColor();
   startBtn.addEventListener("click", startTimer);
 }
@@ -60,66 +63,70 @@ function resetTimer() {
   pauseTimer();
   AnimeReset();
   pausesound();
+  WaterDropStop();
   pauseBtn.removeEventListener("click", pauseTimer);
   ResetColor();
-  AnimeTop = 105;
+  AnimeTop = 250;
   countdown = ResetCnDw;
   Rule();
 }
-function setTimer50m() {
-  clearInterval(timer);
-  resetTimer();
-  countdown = 3000;
-  ResetCnDw = 3000;
-  Rule();
-  SelectFTColor();
-}
+// function setTimer50m() {
+//   clearInterval(timer);
+//   resetTimer();
+//   WaterDropStop();
+//   countdown = 3000;
+//   ResetCnDw = 3000;
+//   Rule();
+//   SelectFTColor();
+// }
 
-function setTimer25m() {
-  clearInterval(timer);
-  resetTimer();
-  countdown = 1500;
-  ResetCnDw = 1500;
-  Rule();
-}
+// function setTimer25m() {
+//   clearInterval(timer);
+//   resetTimer();
+//   WaterDropStop();
+//   countdown = 1500;
+//   ResetCnDw = 1500;
+//   Rule();
+// }
 
-function BreakTime() {
-  clearInterval(timer);
-  if (ResetCnDw === 1500) {
-    ResetCnDw = 300;
-    countdown = 300;
-  } else {
-    ResetCnDw = 600;
-    countdown = 600;
-  }
-  min = parseInt(countdown / 60);
-  sec = countdown % 60;
+// function BreakTime() {
+//   clearInterval(timer);
+//   WaterDropStop();
+//   if (ResetCnDw === 1500) {
+//     ResetCnDw = 300;
+//     countdown = 300;
+//   } else {
+//     ResetCnDw = 600;
+//     countdown = 600;
+//   }
+//   min = parseInt(countdown / 60);
+//   sec = countdown % 60;
 
-  Rule();
-  SelectBrktColor();
-}
+//   Rule();
+//   SelectBrktColor();
+// }
 ///Cal Anime Value : initial top(value) = 105px, target top = -155px, total = 260px box move initial pos
 
-function AnimeRule() {
-  if (AnimeTop < -155) {
-    clearInterval(AnimeWork);
-  } else if (ResetCnDw === 1500) {
-    AnimeTop = AnimeTop - 0.00867;
-    Water.style.top = AnimeTop + "px";
-  } else if (ResetCnDw === 3000) {
-    AnimeTop = AnimeTop - 0.00433;
-    Water.style.top = AnimeTop + "px";
-  } else if (ResetCnDw === 300) {
-    AnimeTop = AnimeTop - 0.04333;
-    Water.style.top = AnimeTop + "px";
-  } else {
-    AnimeTop = AnimeTop - 0.02167;
-    Water.style.top = AnimeTop + "px";
-  }
-}
+// function AnimeRule() {
+//   if (AnimeTop < 0) {
+//     clearInterval(AnimeWork);
+//   } else if (ResetCnDw === 1500) {
+//     AnimeTop = AnimeTop - 0.00833;
+//     Water.style.top = AnimeTop + "px";
+//   } else if (ResetCnDw === 3000) {
+//     AnimeTop = AnimeTop - 0.00417;
+//     Water.style.top = AnimeTop + "px";
+//   } else if (ResetCnDw === 300) {
+//     AnimeTop = AnimeTop - 0.04167;
+//     Water.style.top = AnimeTop + "px";
+//   } else {
+//     AnimeTop = AnimeTop - 0.02;
+//     Water.style.top = AnimeTop + "px";
+//   }
+// }
 
 function AnimeReset() {
-  Water.style.top = "105px";
+  Water.style.top = "250px";
 }
 function playsonud() {
   Alarm.play();
@@ -137,7 +144,7 @@ function pomodorotimer() {
     startBtn.addEventListener("click", startTimer);
     BrkTBtn.addEventListener("click", BreakTime);
     pauseBtn.removeEventListener("click", pauseTimer);
-    countdown = 15;
+    countdown = ResetCnDw;
     playsonud();
     timerDisplay.innerText = "Nice!";
   } else if (countdown < 0) {
@@ -176,39 +183,65 @@ function SelectBrktColor() {
   FcsFTBtn.setAttribute("class", "Thover");
   FcsHTBtn.setAttribute("class", "Thover");
 }
+function WaterDrop() {
+  document.getElementById("DropWater").setAttribute("class", "WaterDrop");
+}
+function WaterDropStop() {
+  document.getElementById("DropWater").setAttribute("class", "WaterDropST");
+}
 
 // !!!TEST Code!!!
 // Activate below codes and Deactivate same formet codes upper, if you want TEST Timer works
-// function setTimer50m() {
-//   clearInterval(timer);
-//   resetTimer();
-//   countdown = 10;
-//   ResetCnDw = 10;
-//   Rule();
-// }
-// function setTimer25m() {
-//   clearInterval(timer);
-//   resetTimer();
-//   countdown = 5;
-//   ResetCnDw = 5;
-//   Rule();
-// }
-// function BreakTime() {
-//   clearInterval(timer);
-//   if (ResetCnDw === 5) {
-//     countdown = 3;
-//   } else {
-//     countdown = 6;
-//   }
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
 
-//   Rule();
-// }
-// function AnimeRule() {
-//   if (AnimeTop < -155) {
-//     clearInterval(AnimeWork);
-//   } else {
-//     AnimeTop = AnimeTop - 0.867;
-//     Water.style.top = AnimeTop + "px";
-//   }
+function AnimeRule() {
+  if (AnimeTop < 0) {
+    clearInterval(AnimeWork);
+  } else if (ResetCnDw === 10) {
+    AnimeTop = AnimeTop - 1.25;
+    Water.style.top = AnimeTop + "px";
+  } else if (ResetCnDw === 16) {
+    AnimeTop = AnimeTop - 0.78125;
+    Water.style.top = AnimeTop + "px";
+  } else if (ResetCnDw === 6) {
+    AnimeTop = AnimeTop - 2.08333;
+    Water.style.top = AnimeTop + "px";
+  } else {
+    AnimeTop = AnimeTop - 3.125;
+    Water.style.top = AnimeTop + "px";
+  }
+}
+function setTimer50m() {
+  clearInterval(timer);
+  resetTimer();
+  WaterDropStop();
+  countdown = 16;
+  ResetCnDw = 16;
+  Rule();
+  SelectFTColor();
+}
+
+function setTimer25m() {
+  clearInterval(timer);
+  resetTimer();
+  WaterDropStop();
+  countdown = 10;
+  ResetCnDw = 10;
+  Rule();
+}
+
+function BreakTime() {
+  clearInterval(timer);
+  WaterDropStop();
+  if (ResetCnDw === 10) {
+    ResetCnDw = 4;
+    countdown = 4;
+  } else {
+    ResetCnDw = 6;
+    countdown = 6;
+  }
+  min = parseInt(countdown / 60);
+  sec = countdown % 60;
+
+  Rule();
+  SelectBrktColor();
+}
