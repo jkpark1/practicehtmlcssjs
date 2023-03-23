1bc10b2c8f33bca7b9c2a9db0dd0d3098793d2f8
jkpark1 <stw369@gmail.com>
2023-03-23 18:44:18 +0900

    release FocusHelper web site

--- pomodoro/index.js
+++ pomodoro/index.js
@@ -1,19 +1,46 @@
 const button = document.querySelectorAll("button");
-const startBtn = button[0];
-const pauseBtn = button[1];
-const resetBtn = button[2];
-const FTBtn = button[3];
-const HTBtn = button[4];
-const BrkTBtn = button[5];
-const icon = document.querySelectorAll(".bi");
+const DayNightBtn = button[0];
+const HelpBtn = button[1];
+const startBtn = button[2];
+const pauseBtn = button[3];
+const resetBtn = button[4];
+const FTBtn = button[5];
+const HTBtn = button[6];
+const BrkTBtn = button[7];
+const closemodalBtn = button[8];
 const timerDisplay = document.getElementById("pomodoroclock");
+const icon = document.querySelectorAll(".bi");
 const Water = document.getElementById("WaterAnime");
+const dropdownmsg = document.getElementById("dropdown-content");
+now_date_time();
+
+function getTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const dayname = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const clock = document.getElementById("nowdate");
+  //clock.innerHTML = hour +":" + minutes + ":"+seconds;
+  clock.innerHTML = `${dayname[day]} ${month + 1}/${date}<br>${
+    hour < 10 ? `0${hour}` : hour
+  }:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+function now_date_time() {
+  setInterval(getTime, 1000);
+}
 ///Timer Rules
 ///initial Value
 // let countdown = 1500;
 // let ResetCnDw = 1500;
-let countdown = 10;
-let ResetCnDw = 10;
+let countdown = 1500;
+let ResetCnDw = 1500;
 let min = "";
 let sec = "";
 min = parseInt(countdown / 60);
@@ -42,6 +69,10 @@ startBtn.addEventListener("click", startTimer);
 FTBtn.addEventListener("click", setTimer50m);
 HTBtn.addEventListener("click", setTimer25m);
 BrkTBtn.addEventListener("click", BreakTime);
+/// Buttons : color option, help modal
+DayNightBtn.addEventListener("click", Dark_Light_mod);
+closemodalBtn.addEventListener("click", Modal_close);
+HelpBtn.addEventListener("click", Modal_open);
 
 function startTimer() {
   timer = setInterval(pomodorotimer, 1000);
@@ -82,60 +113,61 @@ function resetTimer() {
   }
   Rule();
 }
-// function setTimer50m() {
-//   clearInterval(timer);
-//   resetTimer();
-//   WaterDropStop();
-//   countdown = 3000;
-//   ResetCnDw = 3000;
-//   Rule();
-//   SelectFTColor();
-// }
+function setTimer50m() {
+  clearInterval(timer);
+  resetTimer();
+  WaterDropStop();
+  countdown = 3000;
+  ResetCnDw = 3000;
+  Rule();
+  SelectFTColor();
+}
 
-// function setTimer25m() {
-//   clearInterval(timer);
-//   resetTimer();
-//   WaterDropStop();
-//   countdown = 1500;
-//   ResetCnDw = 1500;
-//   Rule();
-// }
+function setTimer25m() {
+  clearInterval(timer);
+  resetTimer();
+  WaterDropStop();
+  ResetColor();
+  countdown = 1500;
+  ResetCnDw = 1500;
+  Rule();
+}
 
-// function BreakTime() {
-//   clearInterval(timer);
-//   WaterDropStop();
-//   if (ResetCnDw === 1500) {
-//     ResetCnDw = 300;
-//     countdown = 300;
-//   } else {
-//     ResetCnDw = 600;
-//     countdown = 600;
-//   }
-//   min = parseInt(countdown / 60);
-//   sec = countdown % 60;
+function BreakTime() {
+  clearInterval(timer);
+  WaterDropStop();
+  if (ResetCnDw === 1500) {
+    ResetCnDw = 300;
+    countdown = 300;
+  } else {
+    ResetCnDw = 600;
+    countdown = 600;
+  }
+  min = parseInt(countdown / 60);
+  sec = countdown % 60;
 
-//   Rule();
-//   SelectBrktColor();
-// }
-///Cal Anime Value : initial top(value) = 105px, target top = -155px, total = 260px box move initial pos
+  Rule();
+  SelectBrktColor();
+}
+//Cal Anime Value : initial top(value) = 105px, target top = -155px, total = 260px box move initial pos
 
-// function AnimeRule() {
-//   if (AnimeTop < 0) {
-//     clearInterval(AnimeWork);
-//   } else if (ResetCnDw === 1500) {
-//     AnimeTop = AnimeTop - 0.00833;
-//     Water.style.top = AnimeTop + "px";
-//   } else if (ResetCnDw === 3000) {
-//     AnimeTop = AnimeTop - 0.00417;
-//     Water.style.top = AnimeTop + "px";
-//   } else if (ResetCnDw === 300) {
-//     AnimeTop = AnimeTop - 0.04167;
-//     Water.style.top = AnimeTop + "px";
-//   } else {
-//     AnimeTop = AnimeTop - 0.02;
-//     Water.style.top = AnimeTop + "px";
-//   }
-// }
+function AnimeRule() {
+  if (AnimeTop < 0) {
+    clearInterval(AnimeWork);
+  } else if (ResetCnDw === 1500) {
+    AnimeTop = AnimeTop - 0.00833;
+    Water.style.top = AnimeTop + "px";
+  } else if (ResetCnDw === 3000) {
+    AnimeTop = AnimeTop - 0.00417;
+    Water.style.top = AnimeTop + "px";
+  } else if (ResetCnDw === 300) {
+    AnimeTop = AnimeTop - 0.04167;
+    Water.style.top = AnimeTop + "px";
+  } else {
+    AnimeTop = AnimeTop - 0.02;
+    Water.style.top = AnimeTop + "px";
+  }
+}
 
 function AnimeReset() {
   Water.style.top = "250px";
@@ -159,7 +191,6 @@ function pomodorotimer() {
     countdown = ResetCnDw;
     playsonud();
     timerDisplay.innerText = "Nice!";
-    setTimeout(resetTimer, 10 * 1000);
   } else if (countdown < 0) {
     timerDisplay.innerText = "Set Time";
   }
@@ -168,14 +199,6 @@ function pomodorotimer() {
   } else if (ResetCnDw != 16) {
     FTBtn.addEventListener("click", setTimer50m);
   }
-}
-function HTButton() {
-  ActBtn = 1;
-  HTBtn.addEventListener("click", setTimer25m);
-}
-function FTButton() {
-  ActBtn = 2;
-  FTBtn.addEventListener("click", setTimer50m);
 }
 
 function Rule() {
@@ -216,60 +239,106 @@ function WaterDrop() {
 function WaterDropStop() {
   document.getElementById("DropWater").setAttribute("class", "WaterDropST");
 }
+function Modal_open() {
+  let modal_T = document.getElementById("modal-layer");
+  let blur_T = document.getElementsByClassName("modalblur");
+  for (let i = 0; i < blur_T.length; i++) {
+    blur_T[i].style.filter = "blur(5px)";
+  }
+  modal_T.style.display = "flex";
+}
+function Modal_close() {
+  let modal_F = document.getElementById("modal-layer");
+  let blur_F = document.getElementsByClassName("modalblur");
+  for (let i = 0; i < blur_F.length; i++) {
+    blur_F[i].style.filter = "none";
+  }
+  modal_F.style.display = "none";
+}
+
+function Dark_Light_mod() {
+  let modeState = document.getElementById("modeIcon").className;
+  let Dark_Light = document.getElementById("modeIcon");
+  if (modeState === "bi bi-moon-fill") {
+    Dark_Light.setAttribute("class", "bi bi-brightness-high-fill");
+    ColorChange("#444", "#222", "whitesmoke");
+  } else if (modeState === "bi bi-brightness-high-fill") {
+    Dark_Light.setAttribute("class", "bi bi-moon-fill");
+    ColorChange("whitesmoke", "#3fabc3", "#555");
+  }
+}
+// function ColorChange_nav(color) {
+//   for (let i = 0; i < G1BackColor.length; i++) {
+//     G1BackColor[i].style.backgroundColor = color;
+//   }
+// }
+function ColorChange(color, color2, color3) {
+  let G1BackColor = document.getElementsByClassName("Group1");
+  let G2BackColor = document.getElementsByClassName("Group2");
+  let Circle_clcok = document.getElementsByClassName("AnimeBox")[0];
+  for (let i = 0; i < G2BackColor.length; i++) {
+    G2BackColor[i].style.backgroundColor = color;
+  }
+  for (let i = 0; i < G1BackColor.length; i++) {
+    G1BackColor[i].style.backgroundColor = color2;
+  }
+  G2BackColor[0].style.color = color3;
+  Circle_clcok.style.borderColor = color3;
+}
 
 // !!!TEST Code!!!
 // Activate below codes and Deactivate same formet codes upper, if you want TEST Timer works
 
-function AnimeRule() {
-  if (AnimeTop < 0) {
-    clearInterval(AnimeWork);
-  } else if (ResetCnDw === 10) {
-    AnimeTop = AnimeTop - 1.25;
-    Water.style.top = AnimeTop + "px";
-  } else if (ResetCnDw === 16) {
-    AnimeTop = AnimeTop - 0.78125;
-    Water.style.top = AnimeTop + "px";
-  } else if (ResetCnDw === 6) {
-    AnimeTop = AnimeTop - 2.08333;
-    Water.style.top = AnimeTop + "px";
-  } else {
-    AnimeTop = AnimeTop - 3.125;
-    Water.style.top = AnimeTop + "px";
-  }
-}
-function setTimer50m() {
-  clearInterval(timer);
-  resetTimer();
-  WaterDropStop();
-  countdown = 16;
-  ResetCnDw = 16;
-  Rule();
-  SelectFTColor();
-}
+// function AnimeRule() {
+//   if (AnimeTop < 0) {
+//     clearInterval(AnimeWork);
+//   } else if (ResetCnDw === 10) {
+//     AnimeTop = AnimeTop - 1.25;
+//     Water.style.top = AnimeTop + "px";
+//   } else if (ResetCnDw === 16) {
+//     AnimeTop = AnimeTop - 0.78125;
+//     Water.style.top = AnimeTop + "px";
+//   } else if (ResetCnDw === 6) {
+//     AnimeTop = AnimeTop - 2.08333;
+//     Water.style.top = AnimeTop + "px";
+//   } else {
+//     AnimeTop = AnimeTop - 3.125;
+//     Water.style.top = AnimeTop + "px";
+//   }
+// }
+// function setTimer50m() {
+//   clearInterval(timer);
+//   resetTimer();
+//   WaterDropStop();
+//   countdown = 16;
+//   ResetCnDw = 16;
+//   Rule();
+//   SelectFTColor();
+// }
 
-function setTimer25m() {
-  clearInterval(timer);
-  resetTimer();
-  WaterDropStop();
-  ResetColor();
-  countdown = 10;
-  ResetCnDw = 10;
-  Rule();
-}
+// function setTimer25m() {
+//   clearInterval(timer);
+//   resetTimer();
+//   WaterDropStop();
+//   ResetColor();
+//   countdown = 10;
+//   ResetCnDw = 10;
+//   Rule();
+// }
 
-function BreakTime() {
-  clearInterval(timer);
-  WaterDropStop();
-  if (ResetCnDw === 10) {
-    ResetCnDw = 4;
-    countdown = 4;
-  } else {
-    ResetCnDw = 6;
-    countdown = 6;
-  }
-  min = parseInt(countdown / 60);
-  sec = countdown % 60;
+// function BreakTime() {
+//   clearInterval(timer);
+//   WaterDropStop();
+//   if (ResetCnDw === 10) {
+//     ResetCnDw = 4;
+//     countdown = 4;
+//   } else {
+//     ResetCnDw = 6;
+//     countdown = 6;
+//   }
+//   min = parseInt(countdown / 60);
+//   sec = countdown % 60;
 
-  Rule();
-  SelectBrktColor();
-}
+//   Rule();
+//   SelectBrktColor();
+// }
